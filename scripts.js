e40857c90425bfcf136e420b7f68a41f42982da9
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-03-30 01:13:32 +0300

    refact

--- scripts.js
+++ scripts.js
@@ -5,18 +5,16 @@ function signOut() {
   });
 }
 
-window.onload = function() {
-  const show = gapi.auth2.getAuthInstance().isSignedIn.get()
-  console.log(show)
-  if (!show) {
-    document.getElementById('sign-in').style.visibility = "visible";
-  }
-};
-
 function onSignIn(googleUser) {
 document.getElementById('sign-in').style.visibility = "hidden";
 document.getElementById('log-out').style.visibility = "visible";
 var auth2 = gapi.auth2.getAuthInstance();
 var profile = googleUser.getBasicProfile();
 document.getElementById('name').innerHTML = profile.getName()
+}
+
+const show = gapi.auth2.getAuthInstance().isSignedIn.get()
+console.log('s')
+if (!show) {
+  document.getElementById('sign-in').style.visibility = "visible";
 }
